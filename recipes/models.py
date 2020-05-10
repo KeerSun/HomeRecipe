f87fa0ea718e8aae2a10129329ed311e453d2516
Keer Sun <keersun@keers-mbp-2.lan>
2020-05-10 10:08:39 -0400

    woo

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class recipe(models.Model):
-    recipe_image = models.ImageField(upload_to = 'images/')
-    recipe_name = models.CharField(max_length = 20)
+    image = models.ImageField(upload_to = 'images/')
+    name = models.CharField(max_length = 20)
+    description = models.CharField(max_length = 60)
     
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import recipe
 
-# Create your views here.
+def home(request):
+    recipes = recipe.objects
+    return render(request,'recipes/home.html',{'recipes':recipes})
